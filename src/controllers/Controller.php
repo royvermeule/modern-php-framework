bcc0366d4ca0938bc4057c84ee21c326f5ec3cf2
Roy Vermeulen <roywilliamvermeulen@gmail.com.com>
2025-08-18 16:12:26 +0200

    add controller with some presentation

---
+++ src/controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Src\controllers;
+
+use Src\core\http\IController;
+use Src\core\http\IsController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class Controller implements IController
+{
+    use IsController;
+
+    public function index(): Response
+    {
+        return new Response('Hello World!');
+    }
+
+    public function getMessage(): Response
+    {
+        return $this->json(['message' => 'Hello World!']);
+    }
+}
