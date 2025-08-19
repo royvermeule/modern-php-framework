d5d7a7d3f3e084f0ddc8f990002520e3a45d720e
Roy Vermeulen <roywilliamvermeulen@gmail.com.com>
2025-08-19 14:55:28 +0200

    add view function to controller and add index view

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -5,14 +5,20 @@ namespace Src\controllers;
 use Src\core\http\IController;
 use Src\core\http\IsController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 final class Controller implements IController
 {
     use IsController;
 
+    /**
+     * @throws SyntaxError
+     * @throws RuntimeError
+     */
     public function index(): Response
     {
-        return new Response('Hello World!');
+        return $this->view('index', ['message' => 'Hello World']);
     }
 
     public function getMessage(): Response
