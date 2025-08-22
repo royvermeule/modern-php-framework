f15afc0abc3b0a54835a2b131a53fb85365b46f7
Roy <roywilliamvermeulen@gmail.com>
2025-08-22 16:09:59 +0200

    quick fix for test db driver

--- tests/config/settings.php
+++ tests/config/settings.php
@@ -3,7 +3,7 @@
 return [
     'TEST_MODE' => true,
     'BASE_URL' => 'http://localhost:8080',
-    'DB_DRIVER' => 'pdo_mysql',
+    'DB_DRIVER' => 'pdo_sqlite',
     'DB_CHARSET' => 'utf8',
     'DB_FILE' => __DIR__ . '/db.sqlite'
 ];
